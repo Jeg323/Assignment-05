ae3eb2a378fa5571483d8e5d9e3940cc99be6fdf
Jeramaya <jeg323@lehigh.edu>
2019-11-15 00:57:48 -0500

    Fixed error in code

--- assignment 5.R
+++ assignment 5.R
@@ -1,10 +1,8 @@
-if (!require("datatable"))install.packages("data.table")
+
+#Run together
+if (!require("data.table")) install.packages("data.table")
 library("data.table")
+#
 
-header <- read.table("Assetbacked.csv", header = TRUE,
-                     sep=",", nrow = 1)
-DF <- fread("Assetbacked.csv", skip=1, sep=",",
-            header=FALSE, data.table=FALSE)
-setnames(DF, colnames(header))
-rm(header)
-
+DF <- fread("Assetbacked.csv", header="auto",
+            data.table=FALSE)
